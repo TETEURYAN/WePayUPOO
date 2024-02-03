a3975282c5ef6b48abe8c0505075e046a111ca89
Matheus Ryan <mrysn@ic.ufal.br>
2024-02-03 11:21:46 -0300

    update src

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import easyaccept.EasyAccept;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        String facade = "br.ufal.ic.p2.wepayu.Facade";
+        EasyAccept.main(new String[]{facade, "tests/us1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us1_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us2.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us2_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us3.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us3_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us4.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us4_1.txt"});
+//
+//        EasyAccept.main(new String[]{facade, "tests/us5.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us5_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us6.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us6_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us7.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us8.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us9.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us9_1.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us10.txt"});
+//        EasyAccept.main(new String[]{facade, "tests/us10_1.txt"});
     }
 }
+
+
